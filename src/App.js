ead5e2048014e6ed4df7104236274c5fdebf7e0c
Shivani Sagar <er.ss1894@gmail.com>
2021-01-01 20:10:01 +0530

    Product_List Component

--- src/App.js
+++ src/App.js
@@ -1,16 +1,34 @@
-//import logo from './logo.svg';
-//import './App.css';
 
-function App() {
-  return (
-    <div className="grid-container">
-      <header>
-        <a href="/">React shopping cart</a>
-      </header>
-      <main>Product list</main>
-      <footer>All right is reserved</footer>
-    </div>
-  );
+import React, { Component } from "react";
+import data from "./data.json";
+import ProductList from "./components/ProductList"
+class App extends Component{
+  constructor() {
+    super()
+    this.state = {
+      products: data.products,
+      size: "",
+      sort: "",
+    };
+  }
+   
+  
+  render() {
+    return (
+      <div className="grid-container">
+        <header>
+          <a href="/">React shopping cart</a>
+        </header>
+        <main>
+          <div className="content">
+            <div className="main"><p><ProductList products={this.state.products} /></p></div>
+            <div className="sidebar">cart</div>
+          </div>
+        </main>
+        <footer>All right is reserved</footer>
+      </div>
+    );
+  }
 }
 
 export default App;
